bbf93fbfc1a523ee943517f0f1a4bdc25d52ec19
OmitHasanAdor <ibneshams05@gmail.com>
2026-09-05 00:02:01 +0600

    solve problem 2-3

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -14,5 +14,31 @@ var containsDuplicate = function (nums) {
 
 // 02. Move Zeroes
 var moveZeroes = function(nums) {
-     // your code goes here      
+    // your code goes here 
+    let write = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[write] = nums[i]
+            write++
+        }
+    }
+    for (let i = write; i < nums.length; i++) {
+        nums[i] = 0
+    }
+}
+// // console.log(moveZeroes([0, 1, 0, 3, 12])) 
+// let nums = [0,0, 1, 0, 3, 12]
+// moveZeroes(nums)
+// console.log(nums)
+
+// 03. Valid Anagram
+var isAnagram = function(s, t) {
+    // your code goes here   
+    s=s.split('').sort().join('')
+    t=t.split('').sort().join('')
+    if (s===t) {
+        return true
+    }
+    return false
 };
+console.log(isAnagram("anagram", "nagaram"))
